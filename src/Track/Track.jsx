e619b4a55c08cd8ef5b0073ac6972dca2127706c
BrianFugate <fugateb@yahoo.com>
2024-01-12 12:04:46 -0600

    Switching icon package to Font Awesome

--- src/Track/Track.jsx
+++ src/Track/Track.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Track.module.css';
-import { PiDotFill } from "react-icons/pi";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
 
 function Track(props) {
     return (
         <div className={styles.track}>
             <p className={styles.song}>{props.track.name}</p>
-            <p className={styles.album}>{props.track.artists[0].name}<PiDotFill className={styles.icon} />{props.track.album.name}</p>
+            <p className={styles.album}>{props.track.artists[0].name} <FontAwesomeIcon className={styles.icon} icon={faCircle} /> {props.track.album.name}</p>
         </div>
     );
 };
--- src/Tracklist/Tracklist.jsx
+++ src/Tracklist/Tracklist.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Track from '../Track/Track.jsx';
 import styles from './Tracklist.module.css';
-import { FaRegSquarePlus, FaRegSquareMinus } from "react-icons/fa6";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquarePlus, faSquareMinus } from '@fortawesome/free-regular-svg-icons'
 
 function Tracklist(props) {
     return (
@@ -11,7 +12,7 @@ function Tracklist(props) {
                                                         <img src={element.album.images[2].url}/>
                                                         <Track track={element}/>
                                                     </div>
-                                                    <button className={styles.button} onClick={() => props.buttonAction(element)}>{props.buttonValue === 'Add' ? <FaRegSquarePlus /> : <FaRegSquareMinus />}</button>
+                                                    <button className={styles.button} onClick={() => props.buttonAction(element)}>{props.buttonValue === 'Add' ? <FontAwesomeIcon icon={faSquarePlus} /> : <FontAwesomeIcon icon={faSquareMinus} />}</button>
                                                 </div>))}
         </>
     );
